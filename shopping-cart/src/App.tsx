00d21765a5622341d4db78becc0ed3215d72df33
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-13 19:16:57 -0300

    feat: initialize shopping cart project with React, TypeScript, and Vite setup

---
+++ shopping-cart/src/App.tsx
@@ -0,0 +1,15 @@
+
+import Button from '@mui/material/Button';
+
+function App() {
+
+  return (
+    <>
+      <h1>Shopping Cart</h1>
+      <Button variant="contained">Add to Cart</Button>
+      
+    </>
+  )
+}
+
+export default App
