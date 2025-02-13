edb23ca7a622369acd32ff913f6778befbd32c4b
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-13 18:17:03 -0300

    feat: add ScoreTracker component for displaying quiz score

---
+++ quiz-app/src/components/ScoreTracker.tsx
@@ -0,0 +1,10 @@
+
+function ScoreTracker() {
+  return (
+    <div className='absolute left-1 top-1 w-[200px] h-[100px] bg-[#FAFAFA] flex items-center justify-center p-4 rounded-lg shadow-lg'>
+      <p className="text-xl font-bold text-[#18181B]">0</p>
+    </div>
+  )
+}
+
+export default ScoreTracker
