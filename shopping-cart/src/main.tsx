4bbcbce6c3ea226ee8b64291664e3cbee624db0c
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-13 19:41:42 -0300

    feat: integrate React Query for data fetching in main application

--- shopping-cart/src/main.tsx
+++ shopping-cart/src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import {QueryClient, QueryClientProvider} from 'react-query'
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
     <App />
+  </QueryClientProvider>
 )
