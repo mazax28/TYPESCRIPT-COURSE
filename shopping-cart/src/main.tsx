c39a90afaba55585da9928ccc313957ff071ffbc
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-13 22:56:39 -0300

    fix: update react-query import to use @tanstack/react-query

--- shopping-cart/src/main.tsx
+++ shopping-cart/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {QueryClient, QueryClientProvider} from 'react-query'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
