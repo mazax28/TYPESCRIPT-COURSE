5d3b4c25a8228c0cea9a0b7d846d0ceca5fba06a
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-02-14 11:19:41 -0300

    feat: refactor App component to utilize useCart hook for improved cart management

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -1,65 +1,45 @@
 import { LinearProgress } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
-import { Products, Product } from './api/types';
+import { Product } from './api/types';
 import { getProducts } from './api/apiProducts';
 import ProductList from './components/ProductList';
-import { useState } from 'react';
 import Drawer from '@mui/material/Drawer';
 import Badge from '@mui/material/Badge';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import CartList from './components/CartList';
+import { useCart } from './hooks/useCart';
 
 
 
 function App() {
-  // Estado del carrito
-  const [cartItems, setCartItems] = useState<Product[]>([]);
-  const [cartOPen, setCartOpen] = useState<boolean>(false);
-
+  const {cartItems, cartOPen,handleAddToCart, handleRemoveFromCart, handleRemoveOneUnit, getTotalItems, handleSetCartOpen} = useCart();
   // Consulta de productos
   const { data, isLoading, error } = useQuery<Product[]>({
     queryKey: ['products'],
     queryFn: getProducts
   });
 
-  // Manejador para agregar productos al carrito
-  // El return es necesario para que setCartItems reciba el nuevo estado del carrito.
-  const handleAddToCart = (clickedItem: Product) => {
-    setCartItems(prev => {
-      const isItemInCart = prev.find(item => item.id === clickedItem.id);
-
-      if (isItemInCart) {
-        return prev.map(item =>
-          item.id === clickedItem.id
-            ? { ...item, amount: (item.amount ?? 1) + 1 }
-            : item
-        );
-      }
-      return [...prev, { ...clickedItem, amount: 1 }];
-    });
-  };
-
-  const getTotalItems = (items: Product[]) => {
-    return items.reduce((total, item) => total + (item.amount ?? 1), 0);
-  }
-
+  
 
   // Renderizado
   return (
-    <div className='bg-[#09090b] min-h-screen w-full px-8 py-8 md:px-16 lg:px-32'>
-      <Drawer anchor='right' open={cartOPen} onClose={() =>setCartOpen(false)} >
-        hola
-      </Drawer>
-      <button className='bg-amber-500' onClick={()=>setCartOpen(true)}>
-        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
-          <AddShoppingCartIcon style={{color:'white'}}/>
-        </Badge>
-      </button>
-      {isLoading ? (
-        <LinearProgress />
-      ) : (
-        data && <ProductList products={data} handleAddToCart={handleAddToCart} />
-      )}
-    </div>
+    <>
+      {isLoading && <LinearProgress />}
+      <div className='bg-[#09090b] min-h-screen w-full px-8 py-8 md:px-16 lg:px-32'>
+        <Drawer anchor='right' open={cartOPen} onClose={() =>handleSetCartOpen(false)} >
+          <CartList cartItems={cartItems} hadleAddToCart={handleAddToCart}  removeFromCart={handleRemoveFromCart} removerOneUnit={handleRemoveOneUnit}/>
+        </Drawer>
+        <button className='bg-[#fafafa]  w-[32px] h-[32px] rounded-lg absolute top-2 right-4 cursor-pointer' onClick={()=>handleSetCartOpen(true)}>
+          <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+            <AddShoppingCartIcon style={{color:'#09090b'}}/>
+          </Badge>
+        </button>
+        {!isLoading && data && (
+          <ProductList products={data} handleAddToCart={handleAddToCart} />
+        )}
+      
+      </div>
+    </>
   );
 }
 
